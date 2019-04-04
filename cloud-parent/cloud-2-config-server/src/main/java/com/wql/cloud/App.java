d4e0f1d769f2ab7a65643fb61898cc311290f003
weiqilong <weiqilong@aspirecn.com>
2019-04-04 15:33:51 +0800

    修改工程描述文件

--- cloud-parent/cloud-2-config-server/src/main/java/com/wql/cloud/App.java
+++ cloud-parent/cloud-2-config-server/src/main/java/com/wql/cloud/App.java
@@ -3,7 +3,6 @@ package com.wql.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
